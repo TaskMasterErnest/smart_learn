de843734b058cb1fda79025564b36d9865733dc8
TaskMasterErnest <kluernest08@gmail.com>
2023-10-29 17:07:38 +0000

    updated test to run ZAP Scan

--- pipeline-scripts/zap.sh
+++ pipeline-scripts/zap.sh
@@ -10,7 +10,11 @@ fail_inprog=$(echo "$zap_output" | grep -oP 'FAIL-INPROG: \K\d+')
 # Check if FAIL-NEW or FAIL-INPROG are not zero, and fail the pipeline
 if [[ $fail_new -ne 0 && $fail_inprog -ne 0 ]]; then
   echo "ZAP scan failed with new or in-progress failures."
+  echo "Zap Security Scan Output:"
+  echo "$zap_output"
   exit 1  # Fail the pipeline
 else
   echo "ZAP scan passed."
+  echo "Zap Security Scan Output:"
+  echo "$zap_output"
 fi
